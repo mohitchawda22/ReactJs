6439628bf4b527769389f15525d61a268c58fa05
Mohit Chawda <87463017+mohitchawda22@users.noreply.github.com>
2024-03-05 23:23:13 +0530

    commit

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+    <div>a bolg with appWrite</div>
+    </>
+  )
+}
+
+export default App
